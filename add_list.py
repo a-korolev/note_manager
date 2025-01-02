46c967ef74de7e6179c59afd4f54f045967c191f
Andrey Korolev <a_korolev@hotmail.com>
2025-01-02 14:03:22 +0500

    add add_list.py file

---
+++ add_list.py
@@ -0,0 +1,9 @@
+# add_list.py
+
+title1 = input('Введите заголовок 1: ')
+title2 = input('Введите заголовок 2: ')
+title3 = input('Введите заголовок 3: ')
+
+title = [title1, title2, title3]
+
+print(title)
