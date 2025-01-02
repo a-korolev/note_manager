50bed4af5d548da09d5faa796e794bcb7309799e
Andrey Korolev <a_korolev@hotmail.com>
2025-01-02 13:04:07 +0500

    add greetings.py file

---
+++ greetings.py
@@ -0,0 +1,16 @@
+# greetings.py
+
+username = 'Andrey Korolev'
+title = 'Note title'
+content = 'Note content...'
+status = 'New'
+created_date = '26.12.2024'
+issue_date = '26.10.2025'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
+
