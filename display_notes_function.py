48e567d344d73ad690d73c549860233725d2a1ed
Andrey Korolev <a_korolev@hotmail.com>
2025-01-16 08:29:18 +0500

    UPDATE: move test data into __name__ == '__main__' block display_notes_function.py

--- display_notes_function.py
+++ display_notes_function.py
@@ -8,69 +8,8 @@ Grade 1. Этап 3. Задание 3.
 Вывод удобен для чтения.
 """
 
-# Список текущих заметок
-notes = [
-    {
-        'ID': 1,
-        'username': 'Иван',
-        'title': 'Учеба',
-        'content': 'Обучение',
-        'status': 'Активна',
-        'created_date': '10-01-2025',
-        'issue_date': '15-01-2025',
-    },
-    {
-        'ID': 2,
-        'username': 'Петр',
-        'title': 'Практика',
-        'content': 'Обучение',
-        'status': 'Активна',
-        'created_date': '26-12-2024',
-        'issue_date': '26-10-2025',
-    },
-    {
-        'ID': 3,
-        'username': 'Иван',
-        'title': 'Рыбалка',
-        'content': 'Отдых',
-        'status': 'Активна',
-        'created_date': '01-01-2025',
-        'issue_date': '08-01-2025',
-    },
-    {
-        'ID': 4,
-        'username': 'Зинаида',
-        'title': 'Учеба',
-        'content': 'Школа',
-        'status': 'Активна',
-        'created_date': '01-09-2024',
-        'issue_date': '25-12-2024',
-    },
-    {
-        'ID': 5,
-        'username': 'Петр',
-        'title': 'Спорт',
-        'content': 'Отдых',
-        'status': 'Активна',
-        'created_date': '01-06-2025',
-        'issue_date': '24-06-2025',
-    },
-]
-
-
-def print_notes_title() -> None:
-    """
-    Функция выводит заголовок списка заметок
-
-    :return: None
-    """
-    print('\nТекущий список заметок:')
-    print('-' * 80)
-
 
 def display_notes(notes: list = None):
-    print_notes_title()
-
     if notes is None:
         notes = []
 
@@ -99,5 +38,64 @@ def display_note(note: dict) -> None:
 
 
 if __name__ == '__main__':
+    # Список текущих заметок
+    notes = [
+        {
+            'ID': 1,
+            'username': 'Иван',
+            'title': 'Учеба',
+            'content': 'Обучение',
+            'status': 'Активна',
+            'created_date': '10-01-2025',
+            'issue_date': '15-01-2025',
+        },
+        {
+            'ID': 2,
+            'username': 'Петр',
+            'title': 'Практика',
+            'content': 'Обучение',
+            'status': 'Активна',
+            'created_date': '26-12-2024',
+            'issue_date': '26-10-2025',
+        },
+        {
+            'ID': 3,
+            'username': 'Иван',
+            'title': 'Рыбалка',
+            'content': 'Отдых',
+            'status': 'Активна',
+            'created_date': '01-01-2025',
+            'issue_date': '08-01-2025',
+        },
+        {
+            'ID': 4,
+            'username': 'Зинаида',
+            'title': 'Учеба',
+            'content': 'Школа',
+            'status': 'Активна',
+            'created_date': '01-09-2024',
+            'issue_date': '25-12-2024',
+        },
+        {
+            'ID': 5,
+            'username': 'Петр',
+            'title': 'Спорт',
+            'content': 'Отдых',
+            'status': 'Активна',
+            'created_date': '01-06-2025',
+            'issue_date': '24-06-2025',
+        },
+    ]
 
+
+    def print_notes_title() -> None:
+        """
+        Функция выводит заголовок списка заметок
+
+        :return: None
+        """
+        print('\nТекущий список заметок:')
+        print('-' * 80)
+
+    print_notes_title()
     display_notes(notes)
